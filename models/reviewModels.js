703bbf957377b2a16feb3b61b2d22095b3ef6098
hossainchisty <hossain.chisty11@gmail.com>
2023-05-29 11:53:42 +0600

    refactor: reviewModels.js for better readability
    Short description: Refactor code for reviewModels.js

--- models/reviewModels.js
+++ models/reviewModels.js
@@ -1,26 +1,28 @@
-const mongoose = require('mongoose');
+// Basic Lib Imports
+const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
     },
     rating: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     comment: {
-        type: String,
+      type: String,
     },
-},
-    { timestamps: true },
-    { versionKey: false });
+  },
+  { timestamps: true },
+  { versionKey: false }
+);
 
-module.exports = mongoose.model('Review', reviewSchema);
-
+module.exports = mongoose.model("Review", reviewSchema);
