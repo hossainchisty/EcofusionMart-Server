fdc9f7d0e809db83ca62030f4c85ad04d28ef8c8
hossainchisty <hossain.chisty11@gmail.com>
2023-05-29 22:10:14 +0600

    feat: Add product model schema definition
    This change adds the schema definition for products which includes various fields such as seller, name, description, price, images, category, brand, stock, and SKU. A pre hook has also been added to generate a unique SKU upon saving records.

---
+++ models/productModels.js
@@ -0,0 +1,66 @@
+// Basic Lib Imports
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  category: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  brand: {
+    type: String,
+    required: false,
+    index: true,
+  },
+  stock: {
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+    remainingStock: {
+      type: Number,
+      default: 0,
+    },
+  },
+  SKU: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+});
+
+productSchema.pre('save', function (next) {
+  const currentDate = new Date().getTime();
+  const random = Math.floor(Math.random() * 10000);
+  this.SKU = `${random}_${currentDate}`;
+  next();
+});
+
+
+module.exports = mongoose.model('Product', productSchema);
+
+
