b2ec6f87ce3aa925db4b6f5f6e7088f9c9e3b4bf
hossainchisty <hossain.chisty11@gmail.com>
2023-05-29 22:13:08 +0600

    feat: Add order models
    Added models for orders with an order item schema to store the quantity, price and product details. The order schema stores user details, order items and order status.

---
+++ models/orderModels.js
@@ -0,0 +1,42 @@
+// Basic Lib Imports
+const mongoose = require('mongoose');
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  // Add more fields as per your requirements (e.g., variations, discounts, etc.)
+});
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  items: [orderItemSchema],
+  total: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'shipped', 'delivered', 'canceled'],
+    default: 'pending',
+  },
+  // Add more fields as per your requirements (e.g., shipping address, payment details, etc.)
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
