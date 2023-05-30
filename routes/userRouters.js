4588e4ae9188c6d5c15f0b38c7b09ce812d7fe40
hossainchisty <hossain.chisty11@gmail.com>
2023-05-30 21:10:57 +0600

    feat: Update userRouters.js to limit password reset request frequency and correct the number of account creation requests.

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,8 +1,8 @@
 // Basic Lib Imports
 const express = require("express");
-
 const router = express.Router();
 const rateLimit = require("express-rate-limit");
+
 const {
   getMe,
 } = require("../controllers/userController");
@@ -12,25 +12,37 @@ const {
   emailVerify,
   loginUser,
   logoutUser,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/authController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 const createAccountLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 5, // Limit each IP to 5 create account requests per `window` (here, per hour)
+  max: 100, // Limit each IP to 100 create account requests per `window` (here, per hour)
   message:
     "Too many accounts created from this IP, please try again after an hour",
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
+const forgetPasswordLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 4, // Limit each IP to 4 create account requests per `window` (here, per hour)
+  message:
+    "Too many password rest mail send from this IP, please try again after an hour",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 // Routing Implement
-// Apply the rate limiting middleware to API calls only
-router.post("/register", registerUser);
-router.post("/verify", emailVerify);
 router.post("/login", loginUser);
-router.post("/logout", protect, logoutUser);
 router.get("/me", protect, getMe);
+router.post("/verify", emailVerify);
+router.post("/logout", protect, logoutUser);
+router.post("/reset-password", resetPassword);
+router.post("/register", createAccountLimiter, registerUser);
+router.post("/forgot-password", forgetPasswordLimiter, forgotPassword);
 
 module.exports = router;
