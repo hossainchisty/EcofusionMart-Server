f860ebe10617c73560e45ab4f31e35e9f98bf3e3
hossainchisty <hossain.chisty11@gmail.com>
2023-05-30 20:59:30 +0600

    feat: Add winston logger to project
    This commit introduces a new logger to the project, utilizing the winston logger library. It includes basic configuration for logging level, format, and transport to a file.

---
+++ config/_winstonLogger.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    // NOTE: This will log the console message
+    // new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/app.log' })
+  ]
+});
+
+module.exports = logger;
