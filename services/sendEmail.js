a61e6cecc89230cd711f4af95cb1afcc7e865244
hossainchisty <hossain.chisty11@gmail.com>
2023-05-30 21:00:01 +0600

    refactor: email service with additional reset password functionality and consolidated email transportation code.
    This commit refactors the email service to include the ability to send password reset links as well as verification emails. The code also consolidates email transportation code for reuse across the two email functions.

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -1,38 +1,60 @@
 // Basic Lib Imports
 require("dotenv").config();
-const nodemailer = require("nodemailer");
+const logger = require('../config/_winstonLogger');
+const nodemailer = require('nodemailer');
 
-// mail service provider's SMTP configuration
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // Set to true if using a secure connection (e.g., SSL/TLS)
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
-
-/**
- *
- * @desc  Function to send the verification email
- *
- */
-const sendVerificationEmail = (email, verificationLink) => {
-  const mailOptions = {
-    from: "eCommerce@example.com",
-    to: email,
-    subject: "Account Verification",
-    text: `Please click the following link to verify your account: ${verificationLink}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending verification email:", error);
-    } else {
-      console.log("Verification email sent:", info.response);
-    }
+const createTransporter = () => {
+  return nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
   });
 };
 
-module.exports = sendVerificationEmail;
+/**
+ * @desc  Function to send the verification email
+ * @param {string} email - Recipient's email address
+ * @param {string} verificationLink - Verification link
+ */
+const sendVerificationEmail = async (email, verificationLink) => {
+  try {
+    const transporter = createTransporter();
+    const mailOptions = {
+      from: "eCommerce@example.com",
+      to: email,
+      subject: "Account Verification",
+      text: `Please click the following link to verify your account: ${verificationLink}`,
+    };
+    const info = await transporter.sendMail(mailOptions);
+    logger.info("Verification email sent:", { response: info.response });
+  } catch (error) {
+    logger.error("Error sending verification email:", error);
+  }
+};
+
+/**
+ * @desc  Function to send the password reset email
+ * @param {string} email - Recipient's email address
+ * @param {string} resetPasswordLink - Password reset link
+ */
+const sendResetPasswordLink = async (email, resetPasswordLink) => {
+  try {
+    const transporter = createTransporter();
+    const mailOptions = {
+      from: "eCommerce@example.com",
+      to: email,
+      subject: 'Password Reset',
+      text: `Please click the following link to reset your password: ${resetPasswordLink}`
+    };
+    const info = await transporter.sendMail(mailOptions);
+    logger.info("Password reset link sent:", { response: info.response });
+  } catch (error) {
+    logger.error("Error sending password reset email:", error);
+  }
+};
+
+module.exports = { sendVerificationEmail, sendResetPasswordLink };
