5de2d721dc02fcdb00efa7fd853b061d5c5d445a
hossainchisty <hossain.chisty11@gmail.com>
2023-05-31 20:35:47 +0600

    refactor: userController to remove getMe function and create userProfile function.
    - Changed the getMe function to userProfile function
    - Modified userProfile to return user details based on its type, excluding sensitive data based on its type.
    - Added error handling for userProfile requests.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,34 @@
 // Basic Lib Import
 const asyncHandler = require("express-async-handler");
-
+const User = require("../models/userModels");
 
 /**
- * @desc    Get user data
+ * @desc    Get user details
  * @route   /api/v1/users/me
  * @method  GET
  * @access  Private
  * @requires Logged User
  */
-const getMe = asyncHandler(async (req, res) => {
-  res.json(req.user);
+const userProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id)
+    .select('-__v')
+    .lean();
+  if (user.isCustomer) {
+    const user = await User.findById(req.user.id)
+    .select('-__v -isSeller -earnings -resetPasswordExpiry -resetPasswordToken -password')
+    .lean();
+    res.status(200).json(user);
+  }
+  
+  if (user.isSeller) {
+    const user = await User.findById(req.user.id)
+    .select('-__v -isCustomer -resetPasswordExpiry -resetPasswordToken -password')
+    .lean();
+    res.status(200).json(user);
+  }
 });
 
 
 module.exports = {
-  getMe
+  userProfile,
 };
