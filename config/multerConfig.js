bc045c4b76ed9b3b34abc2aade26036055e856f8
hossainchisty <hossain.chisty11@gmail.com>
2023-06-01 19:18:56 +0600

    feat: Add cloudinary configuration and multer configuration
    This commit adds the cloudinary configuration to store image files in the cloud using Cloudinary API, and multer configuration that sets up the configurations for saving image files locally on the server.

---
+++ config/multerConfig.js
@@ -0,0 +1,20 @@
+
+// Import the necessary models and dependencies
+const multer = require('multer');
+const path = require('path');
+
+// Set up the multer configuration
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      const extension = path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix + extension);
+    }
+  });
+  
+  const upload = multer({ storage: storage });
+
+  module.exports = upload;
