7c1ff70212b4357ac0fb4741dc0da6815679e155
hossainchisty <hossain.chisty11@gmail.com>
2023-06-03 20:13:57 +0600

    feat: Admin panel can manage user by view and edit user accounts

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,7 +4,7 @@ const User = require("../models/userModels");
 
 /**
  * @desc   Approve seller registration
- * @route  PUT /api/admin/:sellerId/approve
+ * @route  /api/v1/admin/:sellerId/approve
  * @method PUT
  * @access Admin
  * @requires Admin role
@@ -37,7 +37,82 @@ const approveSeller = asyncHandler(async (req, res) => {
 });
 
 
+/**
+ * @desc   Get all users
+ * @route  /api/v1/admin/users
+ * @method GET
+ * @access Admin
+ * @requires Admin role
+ */
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    try {
+        const users = await User.find().select('-__v');
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+/**
+ * @desc   Get a user by ID
+ * @route  /admin/users/:userId
+ * @method GET
+ * @access Admin
+ * @requires Admin role
+ */
+const getUserById = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+
+    }
+});
+
+/**
+ * @desc   Update a user's account details
+ * @route  /admin/users/:userId
+ * @method PUT
+ * @access Admin
+ * @requires Admin role
+ */
+const updateUser = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+    const { full_name, email, phone_number } = req.body;
+
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Update the user's account details
+        user.full_name = full_name;
+        user.email = email;
+        user.phone_number = phone_number;
+
+        await user.save();
+
+        res.status(200).json({ message: 'User account details updated successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 
 module.exports = {
-    approveSeller
+    approveSeller,
+    getAllUsers,
+    getUserById,
+    updateUser,
 }
--- routes/adminRouters.js
+++ routes/adminRouters.js
@@ -3,10 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    approveSeller
+    approveSeller,
+    getAllUsers,
+    getUserById,
 } = require("../controllers/adminController");
 
 // Routing Implement
+router.get("/users", getAllUsers);
 router.put("/:sellerId/approve", approveSeller);
+router.get("/users/:userId", getUserById).put("/users/:userId", getUserById);
 
 module.exports = router;
