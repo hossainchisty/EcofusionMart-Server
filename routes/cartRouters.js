86c2d90a64f6375e8c5132d8c89c537b160198d3
hossainchisty <hossain.chisty11@gmail.com>
2023-06-06 23:43:30 +0600

    feat: Add to cart functionality and get carts lists

---
+++ routes/cartRouters.js
@@ -0,0 +1,17 @@
+// Basic Lib Imports
+const express = require("express");
+const router = express.Router();
+
+const {
+    getCart,
+    addToCart
+} = require("../controllers/cartController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+
+// Routing Implement
+router.get("/getCarts", protect, getCart);
+router.post("/", protect, addToCart);
+
+module.exports = router;
