e1c02224813a5b67642f7b2267061b69e0b2a3ad
hossainchisty <hossain.chisty11@gmail.com>
2023-06-09 14:19:20 +0600

    feat: Added virtual property for taxes and shipping fees and total cost

--- models/cartModels.js
+++ models/cartModels.js
@@ -29,11 +29,36 @@ const cartSchema = new mongoose.Schema(
   { versionKey: false }
 );
 
-// Virtual property for the total price
-cartSchema.virtual("totalPrice").get(function () {
+// Virtual property for the Subtotal 
+cartSchema.virtual("Subtotal").get(function () {
   return this.items.reduce((total, item) => {
     const productPrice = item.product.price || 0;
     return total + productPrice * item.quantity;
+  }, 0);
+});
+
+// Virtual property for the taxes 
+cartSchema.virtual("taxes").get(function () {
+  return this.items.reduce((total, item) => {
+    const taxes = item.product.taxes || 0;
+    return total + taxes * item.quantity;
+  }, 0);
+});
+
+// Virtual property for the shipping fees 
+cartSchema.virtual("shippingFees").get(function () {
+  return this.items.reduce((total, item) => {
+    const shippingFees = item.product.shippingFees || 0;
+    return total + shippingFees * item.quantity;
+  }, 0);
+});
+
+// Virtual property for the total price of the product including shipping, taxes.
+cartSchema.virtual("totalPrice").get(function () {
+  return this.items.reduce((total, item) => {
+    const productPrice = item.product.price || 0;
+    const totalPriceWithTaxesAndFees = (productPrice + item.taxes + item.shippingFees) * item.quantity;
+    return total + totalPriceWithTaxesAndFees;
   }, 0);
 });
 
