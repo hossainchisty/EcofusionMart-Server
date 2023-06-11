160768800db68f4ea08cd9b5a0a9e459ea6450c5
hossainchisty <hossain.chisty11@gmail.com>
2023-06-11 19:56:32 +0600

    chore: db seeding product and user

---
+++ controllers/seedController.js
@@ -0,0 +1,54 @@
+const Product = require('../models/productModels');
+const User = require('../models/userModels');
+const seedUser = require('../seeds/user');
+const seedProduct = require('../seeds/product');
+
+/**
+ * @desc     Inserting three different user roles into the database
+ * @route   /api/seed/users/
+ * @method  GET
+ * @disclaimer This endpoint is experimental don't use in production environments
+ * @author Hossain Chisty
+ */
+
+const seedUsers = async (req, res) => {
+    try {
+        // Delete all existing users
+        await User.deleteMany({});
+
+        // Insert new users
+        const users = await User.insertMany(seedUser.users);
+        res.status(201).json({ message: 'User seeding data inserted successfully', users });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+/**
+ * @desc     Seeding products data in the database
+ * @route   /api/seed/products/
+ * @method  GET
+ * @disclaimer This endpoint is experimental don't use in production environments
+ * @author Hossain Chisty
+ */
+
+const seedProducts = async (req, res) => {
+    try {
+        // Delete all existing products
+        await Product.deleteMany({});
+
+        // Insert new users
+        const products = await Product.insertMany(seedProduct.products);
+        res.status(201).json({ message: 'Product seeding data inserted successfully', products });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    seedUsers,
+    seedProducts,
+}
---
+++ routes/seedRouters.js
@@ -0,0 +1,14 @@
+// Basic Lib Imports
+const express = require("express");
+const seedRouter = express.Router();
+
+const {
+    seedUsers,
+    seedProducts,
+} = require("../controllers/seedController");
+
+// Routing Implement
+seedRouter.get("/users", seedUsers);
+seedRouter.get("/products", seedProducts);
+
+module.exports = seedRouter;
