7a6d5e0b158da6733de72fff3b04e24a162c5013
hossainchisty <hossain.chisty11@gmail.com>
2023-06-14 21:40:10 +0600

    test: Added products seed data

---
+++ seeds/product.js
@@ -0,0 +1,210 @@
+const seedProduct = {
+    products: [
+        {
+            "title": "Apple",
+            "description": "Fresh and juicy apple",
+            "price": 1.99,
+            "taxes": 0.55,
+            "shippingFees": 2.3,
+            "category": "Fruits",
+            "brand": "Farm Fresh",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 50
+            },
+            "images": [
+                "https://cdn.shopify.com/s/files/1/0412/8151/9765/products/25_1080x.jpg?v=1660816037",
+                "https://th.bing.com/th/id/OIP.BlQJNMKQePTXZgWfZlhe3QHaE8?pid=ImgDet&rs=1"
+            ]
+        },
+        {
+            "title": "Banana",
+            "description": "Ripe and yellow banana",
+            "price": 35.99,
+            "taxes": 1.55,
+            "shippingFees": 3.3,
+            "category": "Fruits",
+            "brand": "Organic Farms",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 100
+            },
+            "images": [
+                "https://cdn.shopify.com/s/files/1/0412/8151/9765/products/14_1b19e2a2-d6e3-4ac7-932d-54166a9b4e84_1024x1024.jpg?v=1620642219"
+            ]
+        },
+        {
+            "title": "Orange",
+            "description": "Sweet and tangy orange",
+            "price": 15.49,
+            "taxes": 2.55,
+            "shippingFees": 2.3,
+            "category": "Fruits",
+            "brand": "Citrus Delights",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 75
+            }
+        },
+        {
+            "title": "Mango",
+            "description": "Juicy and tropical mango",
+            "price": 22.49,
+            "taxes": 2.55,
+            "shippingFees": 2.3,
+            "category": "Fruits",
+            "brand": "Exotic Fruits",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 30
+            }
+        },
+        {
+            "title": "Grapes",
+            "description": "Sweet and seedless grapes",
+            "price": 36.99,
+            "taxes": 6.55,
+            "shippingFees": 1.3,
+            "category": "Fruits",
+            "brand": "Vineyard Farms",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 60
+            }
+        },
+        {
+            "title": "Watermelon",
+            "description": "Juicy and refreshing watermelon",
+            "price": 400.99,
+            "taxes": 3.55,
+            "shippingFees": 32.3,
+            "category": "Fruits",
+            "brand": "Farm Fresh",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 20
+            }
+        },
+        {
+            "title": "Pineapple",
+            "description": "Tropical and tangy pineapple",
+            "price": 342.49,
+            "taxes": 2.55,
+            "shippingFees": 21.3,
+            "category": "Fruits",
+            "brand": "Tropical Delights",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 40
+            }
+        },
+        {
+            "title": "Strawberry",
+            "description": "Sweet and succulent strawberry",
+            "price": 223.99,
+            "taxes": 3.55,
+            "shippingFees": 2.3,
+            "category": "Fruits",
+            "brand": "Berry Farms",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 80
+            }
+        },
+        {
+            "title": "Blueberry",
+            "description": "Plump and flavorful blueberry",
+            "price": 43.49,
+            "taxes": 1.25,
+            "shippingFees": 2.3,
+            "category": "Fruits",
+            "brand": "Berry Farms",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 70
+            }
+        },
+        {
+            "title": "Peach",
+            "description": "Juicy and aromatic peach",
+            "price": 13.79,
+            "taxes": 3.55,
+            "shippingFees": 2.3,
+            "category": "Fruits",
+            "brand": "Orchard Fresh",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 45
+            }
+        },
+        {
+            "title": "Cherry",
+            "description": "Sweet and tangy cherry",
+            "price": 3.99,
+            "category": "Fruits",
+            "brand": "Orchard Fresh",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 55
+            }
+        },
+        {
+            "title": "Kiwi",
+            "description": "Exotic and refreshing kiwi",
+            "price": 1.99,
+            "category": "Fruits",
+            "brand": "Tropical Delights",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 25
+            }
+        },
+        {
+            "title": "Pear",
+            "description": "Juicy and crisp pear",
+            "price": 1.49,
+            "category": "Fruits",
+            "brand": "Farm Fresh",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 35
+            }
+        },
+        {
+            "title": "Lemon",
+            "description": "Tangy and citrusy lemon",
+            "price": 0.99,
+            "category": "Fruits",
+            "brand": "Citrus Delights",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 90
+            }
+        },
+        {
+            "title": "Pomegranate",
+            "description": "Sweet and tangy pomegranate",
+            "price": 2.99,
+            "category": "Fruits",
+            "brand": "Exotic Fruits",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 65
+            }
+        },
+        {
+            "title": "Raspberry",
+            "description": "Delicate and flavorful raspberry",
+            "price": 3.49,
+            "category": "Fruits",
+            "brand": "Berry Farms",
+            "stock": {
+                "inStock": true,
+                "remainingStock": 50
+            }
+        }
+
+    ],
+};
+
+module.exports = seedProduct;
