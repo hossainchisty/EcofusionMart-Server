40bcec603057dc96accf8698b2197f13af95a3e3
hossainchisty <hossainchisty@users.noreply.github.com>
2023-06-14 15:17:37 +0000

    style: format files

--- config/_winstonLogger.js
+++ config/_winstonLogger.js
@@ -6,18 +6,17 @@ const logger = winston.createLogger({
     winston.format.timestamp(),
     winston.format.json()
   ),
-  transports: [
-    new winston.transports.File({ filename: "logs/app.log" }),
-  ],
+  transports: [new winston.transports.File({ filename: "logs/app.log" })],
 });
-
 
 // If we're not in production then log to the `console` with the format:
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
 }
 
 module.exports = logger;
