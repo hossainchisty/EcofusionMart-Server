556b6ef70dc36389ed288c318bdc227eb4cb9eee
hossainchisty <hossain.chisty11@gmail.com>
2023-06-16 22:42:16 +0600

    perf: Remove unnecessary try-catch block: Since you're using an asyncHandler middleware, it automatically catches and handles any errors that occur within the async function. Therefore, you can remove the try-catch block.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -40,12 +40,8 @@ const approveSeller = asyncHandler(async (req, res) => {
  */
 
 const getAllUsers = asyncHandler(async (req, res) => {
-  try {
     const users = await User.find().select("-__v");
     res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 /**
