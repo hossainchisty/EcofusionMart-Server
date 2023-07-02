dd2a7691566c64a4f13ae3622673af3674fac110
hossainchisty <hossainchisty@users.noreply.github.com>
2023-07-02 06:46:12 +0000

    style: format files

--- controllers/analysisController.js
+++ controllers/analysisController.js
@@ -3,7 +3,6 @@ const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModels");
 const Order = require("../models/orderModels");
 
-
 /**
  * @desc   Get best-selling products by units sold
  * @route  /api/v1/analysis/best-selling-products?startDate=2023-01-01&endDate=2023-05-31
@@ -13,23 +12,25 @@ const Order = require("../models/orderModels");
  */
 
 const getBestSellingProducts = asyncHandler(async (req, res) => {
-    const { startDate, endDate } = req.query;
+  const { startDate, endDate } = req.query;
 
-    // Group orders by product and calculate total units sold
-    const productSales = await Order.aggregate([
-        { $match: { createdAt: { $gte: new Date(startDate), $lte: new Date(endDate) } } },
-        { $group: { _id: '$product', totalUnitsSold: { $sum: '$quantity' } } },
-        { $sort: { totalUnitsSold: -1 } }
-    ]);
+  // Group orders by product and calculate total units sold
+  const productSales = await Order.aggregate([
+    {
+      $match: {
+        createdAt: { $gte: new Date(startDate), $lte: new Date(endDate) },
+      },
+    },
+    { $group: { _id: "$product", totalUnitsSold: { $sum: "$quantity" } } },
+    { $sort: { totalUnitsSold: -1 } },
+  ]);
 
-    // Retrieve product details for the best-selling products
-    const productIds = productSales.map((productSale) => productSale._id);
-    const bestSellingProducts = await Product.find({ _id: { $in: productIds } });
+  // Retrieve product details for the best-selling products
+  const productIds = productSales.map((productSale) => productSale._id);
+  const bestSellingProducts = await Product.find({ _id: { $in: productIds } });
 
-    res.json({ bestSellingProducts });
+  res.json({ bestSellingProducts });
 });
-
-
 
 /**
  * @desc   Get sales distribution by product categories
@@ -40,42 +41,40 @@ const getBestSellingProducts = asyncHandler(async (req, res) => {
  */
 
 const getSalesByCategory = asyncHandler(async (req, res) => {
+  const salesByCategory = await Order.aggregate([
+    {
+      $lookup: {
+        from: "products",
+        localField: "product",
+        foreignField: "_id",
+        as: "productDetails",
+      },
+    },
+    {
+      $unwind: "$productDetails",
+    },
+    {
+      $group: {
+        _id: "$productDetails.category",
+        totalSales: { $sum: "$quantity" },
+        totalEarnings: { $sum: "$earnings" },
+      },
+    },
+    {
+      $project: {
+        _id: 0, // Exclude the _id field from the results
+        category: "$_id",
+        totalSales: 1,
+        totalEarnings: 1,
+      },
+    },
+    {
+      $sort: { totalSales: -1 }, // Sort by totalSales in descending order
+    },
+  ]);
 
-    const salesByCategory = await Order.aggregate([
-        {
-            $lookup: {
-                from: 'products',
-                localField: 'product',
-                foreignField: '_id',
-                as: 'productDetails'
-            }
-        },
-        {
-            $unwind: '$productDetails'
-        },
-        {
-            $group: {
-                _id: '$productDetails.category',
-                totalSales: { $sum: '$quantity' },
-                totalEarnings: { $sum: '$earnings' }
-            }
-        },
-        {
-            $project: {
-                _id: 0, // Exclude the _id field from the results
-                category: '$_id',
-                totalSales: 1,
-                totalEarnings: 1
-            }
-        },
-        {
-            $sort: { totalSales: -1 } // Sort by totalSales in descending order
-        }
-    ]);
-
-    res.json(salesByCategory);
+  res.json(salesByCategory);
 });
-
 
 /**
  * @desc   Calculating the earnings in the last 30 days
@@ -86,31 +85,30 @@ const getSalesByCategory = asyncHandler(async (req, res) => {
  */
 
 const getLastMonthEarnings = asyncHandler(async (req, res) => {
-    const thirtyDaysAgo = new Date();
-    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+  const thirtyDaysAgo = new Date();
+  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
-    const result = await Order.aggregate([
-        {
-            $match: {
-                createdAt: { $gte: thirtyDaysAgo }
-            }
-        },
-        {
-            $group: {
-                _id: null,
-                totalEarnings: { $sum: '$earnings' }
-            }
-        }
-    ]);
+  const result = await Order.aggregate([
+    {
+      $match: {
+        createdAt: { $gte: thirtyDaysAgo },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalEarnings: { $sum: "$earnings" },
+      },
+    },
+  ]);
 
-    const earnings = result.length > 0 ? result[0].totalEarnings : 0;
+  const earnings = result.length > 0 ? result[0].totalEarnings : 0;
 
-    res.json({ earnings });
+  res.json({ earnings });
 });
 
-
 module.exports = {
-    getLastMonthEarnings,
-    getSalesByCategory,
-    getBestSellingProducts,
-}
+  getLastMonthEarnings,
+  getSalesByCategory,
+  getBestSellingProducts,
+};
