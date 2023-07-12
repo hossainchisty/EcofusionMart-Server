77d2f6d245e7f6224f4b67a7651c742915fd9e23
hossainchisty <hossain.chisty11@gmail.com>
2023-07-12 23:09:40 +0600

    chore: mongoose debug mode off

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URL);
-    mongoose.set("debug", true);
+    mongoose.set("debug", false);
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
     console.log(error);
