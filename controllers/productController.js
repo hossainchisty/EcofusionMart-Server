7eb28ad95b9054414d1938ea0586d466b9f30def
hossainchisty <hossainchisty@users.noreply.github.com>
2023-08-13 13:05:29 +0000

    style: format files

--- controllers/productController.js
+++ controllers/productController.js
@@ -102,9 +102,9 @@ const searchProducts = asyncHandler(async (req, res) => {
 
     if (products.length === 0) {
       return res.status(404).json({
-        status: 'error',
+        status: "error",
         code: 404,
-        message: 'No products found.',
+        message: "No products found.",
       });
     }
 
@@ -116,16 +116,16 @@ const searchProducts = asyncHandler(async (req, res) => {
 
     cache.set(cacheKey, productsWithoutSubdocuments);
 
-    res.status(200).json({ 
-      status: 'success',
+    res.status(200).json({
+      status: "success",
       code: 200,
       result: productsWithoutSubdocuments,
     });
   } catch (error) {
     res.status(500).json({
-      status: 'error',
+      status: "error",
       code: 500,
-      message: 'Internal server error.',
+      message: "Internal server error.",
     });
   }
 });
